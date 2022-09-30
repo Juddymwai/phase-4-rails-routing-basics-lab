31132620afcb287dff9e9f4c261fa9365943f133
Judy Mwai <juddiemukami@gmail.com>
2022-09-30 13:54:41 +0300

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -5,10 +5,13 @@ class StudentsController < ApplicationController
 
     end
     def grades
+        # In the grades action, return JSON data representing a list of all students, ordered by grade from highest to lowest
         students = Student.order(grade: :desc)
         render json:students
     end
     def highest_grade 
+        # In the highest_grade action, return JSON data representing the one student with the highest grade
+        
         students =Student.order(grade: :desc).first
         render json:students
     end
